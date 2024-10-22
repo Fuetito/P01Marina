316ab3a6a5ed36199fa92eda6469a8a5caea3b94
Elena UPC <elena.fernandez.leon@estudiantat.upc.edu>
2024-10-22 12:08:47 +0200

    EscenaPersonaje

---
+++ Assets/Water.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Water : MonoBehaviour
+{
+    /*public float Density = 1;
+    private void OnTriggerEnter(Collider other)
+    {
+        var Slower = other.GetComponent<slower>();     //el GetComponent mira de cualquier objeto.
+        if (Slower != null)
+        {
+            Slower.SlowDown(Density);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)      //Cuando salga del agua
+    {
+        var Slower = other.GetComponent<slower>();
+        if (Slower != null)
+        {
+            Slower.Reset();
+        }
+    }*/
+}
