f26e0053a7ef1cfed7115f30986c917a305e97a3
eleniss <elena.fernandez.leon@estudiantat.upc.edu>
2024-10-23 21:39:36 +0200

    intentoCaminar

--- Assets/InputController.cs
+++ Assets/InputController.cs
@@ -6,17 +6,13 @@ using UnityEngine.InputSystem;
 
 public class InputController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    
     Vector2 _move;
     public Vector2 Move => _move;
 
     public bool _jump;
     public bool Jump => _jump;
-    //misma funcion solo con una linea de codigo
-    //public Vector2 GetMove()
-    //{
-    //    return _move;
-    //}
+   
     private void OnMove(InputValue input)
     {
         _move = input.Get<Vector2>();
--- Assets/pitilin.cs
+++ Assets/pitilin.cs
@@ -15,8 +15,8 @@ public class pitilin : MonoBehaviour
 
     //VELOCITAT CAMINAR
     public float Speed = 1;
-    public float JumpSpeed = 10;
-    public float AirControl;
+   public float JumpSpeed = 10;
+    public float AirControl=10;
 
     //VELOCITAT ROTACIÓ
     public float TurnSpeed = 0.1f;
@@ -28,64 +28,93 @@ public class pitilin : MonoBehaviour
         _input = GetComponent<InputController>();
         _groundChecker = GetComponentInChildren<GroundChecker>();
     }
-
   
     void Update()
     {
         Move();
     }
 
-    private bool ShouldJump()
-    {
-        return _input.Jump && _groundChecker.Grounded;                 //SALTARRRR
-    }
-
-    private void Jump(ref Vector3 vel)
-    {
-        vel.y = JumpSpeed;
-    }
-
     private void Move()
     {
-        var localInput = transform.right * _input.Move.x + transform.forward * _input.Move.y;  //POV PLAYER
-        //esto es simple move
+        var localInput = transform.right * _input.Move.x + transform.forward * _input.Move.y;
         Vector3 direction = new Vector3(localInput.x, 0, localInput.z);
-        //_characterController.SimpleMove(direction * Speed);
 
         Vector3 vel = new Vector3();
-        float smoothFactor = _groundChecker.Grounded? 1 : AirControl * Time.deltaTime;
+        float smoothFactor = _groundChecker.Grounded ? 1 : AirControl * Time.deltaTime;
 
-        
         vel.x = Mathf.Lerp(_lastVelocity.x, direction.x * Speed, smoothFactor);
         vel.y = _lastVelocity.y;
         vel.z = Mathf.Lerp(_lastVelocity.z, direction.z * Speed, smoothFactor);
-        //not simple move
 
-        vel.y = GetGravity();     //Aplicas una vel, guardas
-        if (ShouldJump())                   //SALTARRCH
-            Jump(ref vel);
+        vel.y = GetGravity();
 
-        Debug.Log(_input.Move.x);
         _characterController.Move(vel * Time.deltaTime);
 
-
-        
-        //turn around
-        if (direction.magnitude > 0)
-        {
-            Vector3 target = transform.position + direction;
-            Vector3 current = transform.position + transform.forward;
-            Vector3 look = Vector3.Lerp(current, target, TurnSpeed * Time.deltaTime);
-
-            transform.LookAt(target);
-
-        }
-        _lastVelocity = vel;      //la ultima vel del frame(?
+        _lastVelocity = vel;
     }
-
     private float GetGravity()
     {
         return _lastVelocity.y + Physics.gravity.y * Time.deltaTime;
     }
+
+
+
+
+
+
+
+    /*
+        private bool ShouldJump()
+        {
+            return _input.Jump && _groundChecker.Grounded;                 //SALTARRRR
+        }
+
+        private void Jump(ref Vector3 vel)
+        {
+            vel.y = JumpSpeed;
+        }
+
+        private void Move()
+        {
+            var localInput = transform.right * _input.Move.x + transform.forward * _input.Move.y;  //POV PLAYER
+            //esto es simple move
+            Vector3 direction = new Vector3(localInput.x, 0, localInput.z);
+            //_characterController.SimpleMove(direction * Speed);
+
+            Vector3 vel = new Vector3();
+            float smoothFactor = _groundChecker.Grounded? 1 : AirControl * Time.deltaTime;
+
+
+            vel.x = Mathf.Lerp(_lastVelocity.x, direction.x * Speed, smoothFactor);
+            vel.y = _lastVelocity.y;
+            vel.z = Mathf.Lerp(_lastVelocity.z, direction.z * Speed, smoothFactor);
+            //not simple move
+
+            vel.y = GetGravity();     //Aplicas una vel, guardas
+            if (ShouldJump())                   //SALTARRCH
+                Jump(ref vel);
+
+
+            _characterController.Move(vel * Time.deltaTime);
+
+
+
+            //turn around
+            if (direction.magnitude > 0)
+            {
+                Vector3 target = transform.position + direction;
+                Vector3 current = transform.position + transform.forward;
+                Vector3 look = Vector3.Lerp(current, target, TurnSpeed * Time.deltaTime);
+
+                transform.LookAt(target);
+
+            }
+            _lastVelocity = vel;      //la ultima vel del frame(?
+        }
+
+        private float GetGravity()
+        {
+            return _lastVelocity.y + Physics.gravity.y * Time.deltaTime;
+        }*/
 
 }
