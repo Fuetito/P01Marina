393e5b4239ef2d8b364938511fa5d98fe59c1f6e
Marina UPC <marina.martin.prieto@estudiantat.upc.edu>
2024-10-23 09:25:11 +0200

    Water y pendulos

--- Assets/Water.cs
+++ Assets/Water.cs
@@ -2,24 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Water : MonoBehaviour
+public class water : MonoBehaviour
 {
-    /*public float Density = 1;
+    public float Density = 1;
     private void OnTriggerEnter(Collider other)
     {
-        var Slower = other.GetComponent<slower>();     //el GetComponent mira de cualquier objeto.
+
+        var Slower = other.GetComponent<slower>();
         if (Slower != null)
         {
             Slower.SlowDown(Density);
         }
     }
 
-    private void OnTriggerExit(Collider other)      //Cuando salga del agua
+    private void OnTriggerExit(Collider other)
     {
         var Slower = other.GetComponent<slower>();
         if (Slower != null)
         {
             Slower.Reset();
         }
-    }*/
+    }
 }
--- Assets/slower.cs
+++ Assets/slower.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class slower : MonoBehaviour
 {
-    // Start is called before the first frame update
+    Rigidbody _rigidbody;
+
+    private float _orgDrag;
+    private float _orgAngularDrag;
+
+    public float AngularDragEffect = 10;
+    public float DragEffect = 5;
+    public float VelocityEffect = 0.5f;
+
     void Start()
     {
-        
+        _rigidbody = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+
+    }
+    internal void SlowDown(float density)
+    {
+        _orgDrag = _rigidbody.drag; //nos guardamos los valores iniciales para después poder volver a darlos en el reset.
+        _orgAngularDrag = _rigidbody.angularDrag;
+
+        _rigidbody.velocity *= Mathf.Min(1, (VelocityEffect / density));
+        _rigidbody.angularDrag = (AngularDragEffect * density);
+        _rigidbody.drag = DragEffect * density;
+
+
+    }
+
+    internal void Reset()
+    {
+        _rigidbody.angularDrag = _orgAngularDrag;
+        _rigidbody.drag = _orgDrag;
     }
 }
